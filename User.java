520ee8eff6e851c7a8b9af1087a94d156972ece0
sharvali-ladekar <sladekar@purdue.edu>
2024-10-30 13:40:00 -0400

    Update User.java

--- User.java
+++ User.java
@@ -1,14 +1,4 @@
 public class User {
-  private int ID;
 
-  public User () {}
-
-    public int getID() {
-      return ID;
-    }
-
-    public void setID(int ID) {
-      this.ID = ID;
-    }
 }
 
