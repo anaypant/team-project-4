b2e53c36481d4183b025f8deaf4edbb772da6875
Mentor13 <86921532+Mentor13@users.noreply.github.com>
2024-10-30 13:59:16 -0400

    Update PostInterface.java

--- PostInterface.java
+++ PostInterface.java
@@ -1,8 +1,12 @@
 public interface PostInterface {
   private void createPost(String content);
-  private void deletePost(String content);
-  private void upvotePost;
-  private void downvotePost;
+  private void createPost(String content, String image);
+  private void deletePost(Post post);
+  private void upvotePost(Post post);
+  private void downvotePost(Post post);
+  private ArrayList<Post> getPosts();
+  private void getUpvotes();
+  private void getDownvotes();
   private void addComment(String comment);
   private void deleteComment(String comment);
 }
