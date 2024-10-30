4dc806bdf33ef0c3425734fa165e6c67165565d4
anaypant <anaypant212@gmail.com>
2024-10-30 13:20:48 -0400

    Added RunLocalTest

---
+++ RunLocalTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+public class RunLocalTest {
+    @Test(timeout = 1000)
+    public void test() {
+
+        // Testing
+        PostDatabase testDB = new PostDatabase();
+
+    }
+}
