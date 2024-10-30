df0b34851aa8b7e0478cf2b0e81bfbb5df095e8b
sharvali-ladekar <sladekar@purdue.edu>
2024-10-30 13:54:54 -0400

    Update PostInterface.java

--- PostInterface.java
+++ PostInterface.java
@@ -1,2 +1,8 @@
 public interface PostInterface {
+  private void createPost(String content);
+  private void deletePost(String content);
+  private void upvotePost;
+  private void downvotePost;
+  private void addComment(String comment);
+  private void deleteComment(String comment);
 }
