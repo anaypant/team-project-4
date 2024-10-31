bb171031e8e7ae1bb2686eff4855dc1a536e0d17
Utsav677 <arora252@purdue.edu>
2024-10-31 10:00:47 -0400

    Update PostDatabase.java
    implmenting interface

--- PostDatabase.java
+++ PostDatabase.java
@@ -1,2 +1,26 @@
 public class PostDatabase implements PostInterface{
+  private void createPost(String content){
+    
+  }
+  private void createPost(String content, String image){
+    
+  }
+  private void deletePost(Post post){
+    
+  }
+  private void upvotePost(Post post){
+    
+  }
+  private void downvotePost(Post post){
+    
+  }
+  private ArrayList<Post> getPosts(){
+    return null;
+  }
+  private void getUpvotes(){
+    re
+  }
+  private void getDownvotes();
+  private void addComment(String comment);
+  private void deleteComment(String comment);
 }
