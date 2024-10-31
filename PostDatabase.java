4ef8729f6ebcaa34d481ff579dbaaf6c89ed000e
Utsav677 <arora252@purdue.edu>
2024-10-31 10:05:28 -0400

    Update PostDatabase.java
    implmenting PostInterface into Database

--- PostDatabase.java
+++ PostDatabase.java
@@ -2,25 +2,42 @@ public class PostDatabase implements PostInterface{
   private void createPost(String content){
     
   }
+  
   private void createPost(String content, String image){
     
   }
+  
   private void deletePost(Post post){
     
   }
+  
   private void upvotePost(Post post){
     
   }
+  
   private void downvotePost(Post post){
     
   }
+  
   private ArrayList<Post> getPosts(){
     return null;
   }
-  private void getUpvotes(){
-    re
+  
+  private int getUpvotes(){
+    
+    return 0;
   }
-  private void getDownvotes();
-  private void addComment(String comment);
-  private void deleteComment(String comment);
+  
+  private int getDownvotes(){
+    
+    return 0;
+  }
+  
+  private void addComment(String comment){
+    
+  }
+  
+  private void deleteComment(String comment){
+    
+  }
 }
